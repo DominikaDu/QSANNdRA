a5ded4ad449cb1d5c30193bd6f22d05d35691567
DominikaDu <dominika.durovcikova@gmail.com>
2020-02-20 11:22:10 +0000

    initial commit for qsanndra routines

---
+++ example_training.py
@@ -0,0 +1,19 @@
+import QSANNdRA as Q
+
+# Fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+training_spectra = 'data/dataframes/training_spectra_cleaned.csv'
+training_norms = 'data/dataframes/training_spectra_norms_cleaned.csv'
+path_to_models = 'models/preprocessing_models/'
+
+scaler_red_one, pca_red, scaler_red_two, train_red, test_red, \
+    scaler_blue_one, pca_blue, scaler_blue_two, train_blue, test_blue = \
+        Q.prepare_training_data(training_spectra=training_spectra,training_norms=training_norms,models_save=True,path_to_models=path_to_models)
+
+# train full committee
+
+# inverse transform training sets and predictions
+
+# evaluate performance
