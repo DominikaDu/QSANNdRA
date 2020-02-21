5f9fbe37fb0e4b13f468c41bd48f153cdcefbd90
DominikaDu <dominika.durovcikova@gmail.com>
2020-02-21 11:00:15 +0000

    added a note

--- example_training.py
+++ example_training.py
@@ -10,3 +10,5 @@ training_norms = 'data/dataframes/good/training_spectra_norms_cleaned.csv'
 path_to_models = 'models/'
 
 _, _ = Q.get_QSANNdRA(training_spectra=training_spectra,training_norms=training_norms,models_save=True,path_to_models=path_to_models,C=100,load=False,save_errs=True)
+
+# Add error plotting
